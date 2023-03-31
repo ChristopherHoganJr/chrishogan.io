7c0decb05358a7fb55579797c6d770dfc89d8e01
Chris <55262451+ChristopherHoganJr@users.noreply.github.com>
2023-03-31 16:10:08 -0700

    Update ExperienceSection.tsx

--- app/components/landing/ExperienceSection.tsx
+++ app/components/landing/ExperienceSection.tsx
@@ -14,21 +14,54 @@ const ExperienceSection: FC = () => {
         <h2 className='font-bold text-4xl text-[var(--color-black)] text-center'>
           Crafting Digital Experiences
         </h2>
-        <div className='grid grid-cols-1 md:grid-cols-[1fr_2fr] gap-10'>
-          <p className='font-xl font-semibold text-center md:text-left'>
-            As a full stack developer with expertise in Java, JavaScript,
-            TypeScript, and Python, I pride myself on my ability to create
-            clean, efficient code and solve complex problems with innovative
-            solutions. My experience has taught me to approach development from
-            both a technical and user-centered perspective, ensuring that the
-            final product is both functional and intuitive. I am passionate
-            about staying up to date with the latest technologies and techniques
-            and constantly challenge myself by practicing coding challenges in
-            my free time. Whether it's front-end design or back-end development,
-            I am confident in my ability to deliver high-quality, scalable
-            solutions that meet the needs of clients and users alike.
-          </p>
-          <div className="bg-[url('/images/codeface.jpg')] w-full h-full bg-cover bg-center  bg-no-repeat "></div>
+        <div className='grid grid-cols-1 md:grid-cols-2 justify-items-center align-middle gap-4'>
+          <div className='flex flex-col gap-4'>
+            <p className='text-lg font-semibold text-left'>
+              As a full stack developer with expertise in Java, JavaScript,
+              TypeScript, and Python, I pride myself on my ability to create
+              clean, efficient code and solve complex problems with innovative
+              solutions. My experience has taught me to approach development
+              from both a technical and user-centered perspective, ensuring that
+              the final product is both functional and intuitive. I am
+              passionate about staying up to date with the latest technologies
+              and techniques and constantly challenge myself by practicing
+              coding challenges in my free time. Whether it's front-end design
+              or back-end development, I am confident in my ability to deliver
+              high-quality, scalable solutions that meet the needs of clients
+              and users alike.
+            </p>
+
+            <div>
+              <h3 className='text-xl font-bold text-left'>
+                Some Stacks I Use:
+              </h3>
+
+              <ul>
+                <li className='text-lg font-semibold text-left'>
+                  Java with Spring Boot - enterprise-level applications that
+                  require high performance and reliability.
+                </li>
+                <li className='text-lg font-semibold text-left'>
+                  Python with Flask - lightweight web framework that allows
+                  developers to build web applications quickly and easily.
+                </li>
+                <li className='text-lg font-semibold text-left'>
+                  MERN - full-stack JavaScript framework that includes MongoDB,
+                  Express.js, React, and Node.js.
+                </li>
+                <li className='text-lg font-semibold text-left'>
+                  Next.js - React-based open-source framework that allows
+                  developers to build server-side rendered React applications
+                  with ease
+                </li>
+                <li className='text-lg font-semibold text-left'>
+                  T3 - full-stack TypeScript framework that includes Next.js,
+                  tRPC, Prisma, and NextAuth.
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="bg-[url('/images/codeface.jpg')] w-full h-[600px]  bg-cover bg-center  bg-no-repeat top-32 sticky "></div>
         </div>
       </div>
     </section>
