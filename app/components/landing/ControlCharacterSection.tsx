be0976fa53cf0eb0fd4df004af6db11fb9e37f2b
Chris <55262451+ChristopherHoganJr@users.noreply.github.com>
2023-03-30 17:48:54 -0700

    Update ControlCharacterSection.tsx

--- app/components/landing/ControlCharacterSection.tsx
+++ app/components/landing/ControlCharacterSection.tsx
@@ -70,7 +70,7 @@ const ControlCharacterSection: FC = () => {
           onClick={moveLeft}
           src='/images/arrow.png'
           alt='up arrow'
-          className='scale-75 -rotate-90 -translate-y-2 transition-all active:translate-y-0 drop-shadow-[-10px_0_5px_rgba(0,0,0,0.5)] active:drop-shadow-[0px_0_5px_rgba(0,0,0,0.5)] active:scale-[70%] hover:scale-[80%] hover:-translate-y-4 hover:drop-shadow-[-18px_0_5px_rgba(0,0,0,0.5)]'
+          className='scale-75 -rotate-90 -translate-y-2 transition-all active:translate-y-0 md:drop-shadow-[-10px_0_5px_rgba(0,0,0,0.5)] active:drop-shadow-[0px_0_5px_rgba(0,0,0,0.5)] active:scale-[70%] md:hover:scale-[80%] md:hover:-translate-y-4 md:hover:drop-shadow-[-18px_0_5px_rgba(0,0,0,0.5)]'
         />
         <img
           src='/images/arrow.png'
@@ -82,13 +82,13 @@ const ControlCharacterSection: FC = () => {
           onClick={moveRight}
           src='/images/arrow.png'
           alt='up arrow'
-          className='scale-75 rotate-90 -translate-y-2 transition-all active:translate-y-0 drop-shadow-[10px_0_5px_rgba(0,0,0,0.5)] active:drop-shadow-[0px_0_5px_rgba(0,0,0,0.5)] active:scale-[70%] hover:scale-[80%] hover:-translate-y-4 hover:drop-shadow-[18px_0_5px_rgba(0,0,0,0.5)]'
+          className='scale-75 rotate-90 -translate-y-2 transition-all active:translate-y-0 md:drop-shadow-[10px_0_5px_rgba(0,0,0,0.5)] active:drop-shadow-[0px_0_5px_rgba(0,0,0,0.5)] active:scale-[70%] md:hover:scale-[80%] md:hover:-translate-y-4 md:hover:drop-shadow-[18px_0_5px_rgba(0,0,0,0.5)]'
         />
         <img
           src='/images/arrow.png'
           onClick={moveDuck}
           alt='up arrow'
-          className='scale-75 rotate-180 -translate-y-2 transition-all active:translate-y-0 drop-shadow-[0_-10px_5px_rgba(0,0,0,0.5)] active:drop-shadow-[0px_0_5px_rgba(0,0,0,0.5)] active:scale-[70%] hover:scale-[80%] hover:-translate-y-4 hover:drop-shadow-[0_-18px_5px_rgba(0,0,0,0.5)]'
+          className='scale-75 rotate-180 -translate-y-2 transition-all active:translate-y-0 md:drop-shadow-[0_-10px_5px_rgba(0,0,0,0.5)] active:drop-shadow-[0px_0_5px_rgba(0,0,0,0.5)] active:scale-[70%] md:hover:scale-[80%] md:hover:-translate-y-4 md:hover:drop-shadow-[0_-18px_5px_rgba(0,0,0,0.5)]'
         />
       </div>
     </section>
