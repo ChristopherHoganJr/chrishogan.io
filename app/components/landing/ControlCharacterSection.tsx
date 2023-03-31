c7c479c67bfaa13b5c6d2262a4f1389fc83ccafe
Chris <55262451+ChristopherHoganJr@users.noreply.github.com>
2023-03-30 20:57:49 -0700

    updated

--- app/components/landing/ControlCharacterSection.tsx
+++ app/components/landing/ControlCharacterSection.tsx
@@ -66,30 +66,38 @@ const ControlCharacterSection: FC = () => {
         </div>
       </div>
       <div className='flex flex-wrap justify-center'>
-        <img
-          onClick={moveLeft}
-          src='/images/arrow.png'
-          alt='up arrow'
-          className='scale-75 -rotate-90 -translate-y-2 transition-all active:translate-y-0 md:drop-shadow-[-10px_0_5px_rgba(0,0,0,0.5)] active:drop-shadow-[0px_0_5px_rgba(0,0,0,0.5)] active:scale-[70%] md:hover:scale-[80%] md:hover:-translate-y-4 md:hover:drop-shadow-[-18px_0_5px_rgba(0,0,0,0.5)]'
-        />
-        <img
-          src='/images/arrow.png'
-          onClick={moveJump}
-          alt='up arrow'
-          className='scale-75 -translate-y-2 transition-all active:translate-y-0 md:drop-shadow-[0_10px_5px_rgba(0,0,0,0.5)] active:drop-shadow-[0px_0_5px_rgba(0,0,0,0.5)] active:scale-[70%] md:hover:scale-[80%] md:hover:-translate-y-4 md:hover:drop-shadow-[0_18px_5px_rgba(0,0,0,0.5)]'
-        />
-        <img
-          onClick={moveRight}
-          src='/images/arrow.png'
-          alt='up arrow'
-          className='scale-75 rotate-90 -translate-y-2 transition-all active:translate-y-0 md:drop-shadow-[10px_0_5px_rgba(0,0,0,0.5)] active:drop-shadow-[0px_0_5px_rgba(0,0,0,0.5)] active:scale-[70%] md:hover:scale-[80%] md:hover:-translate-y-4 md:hover:drop-shadow-[18px_0_5px_rgba(0,0,0,0.5)]'
-        />
-        <img
-          src='/images/arrow.png'
-          onClick={moveDuck}
-          alt='up arrow'
-          className='scale-75 rotate-180 -translate-y-2 transition-all active:translate-y-0 md:drop-shadow-[0_-10px_5px_rgba(0,0,0,0.5)] active:drop-shadow-[0px_0_5px_rgba(0,0,0,0.5)] active:scale-[70%] md:hover:scale-[80%] md:hover:-translate-y-4 md:hover:drop-shadow-[0_-18px_5px_rgba(0,0,0,0.5)]'
-        />
+        <div className=''>
+          <img
+            onClick={moveLeft}
+            src='/images/arrow.png'
+            alt='up arrow'
+            className='scale-75 -rotate-90 -translate-y-2 transition-all active:translate-y-0 drop-shadow-[-10px_0_5px_rgba(0,0,0,0.5)] active:drop-shadow-[0px_0_5px_rgba(0,0,0,0.5)] active:scale-[70%] hover:scale-[80%] hover:-translate-y-4 hover:drop-shadow-[-18px_0_5px_rgba(0,0,0,0.5)]'
+          />
+        </div>
+        <div className=''>
+          <img
+            src='/images/arrow.png'
+            onClick={moveJump}
+            alt='up arrow'
+            className='scale-75 -translate-y-2 transition-all active:translate-y-0 md:drop-shadow-[0_10px_5px_rgba(0,0,0,0.5)] active:drop-shadow-[0px_0_5px_rgba(0,0,0,0.5)] active:scale-[70%] hover:scale-[80%] hover:-translate-y-4 hover:drop-shadow-[0_18px_5px_rgba(0,0,0,0.5)]'
+          />
+        </div>
+        <div className=''>
+          <img
+            onClick={moveRight}
+            src='/images/arrow.png'
+            alt='up arrow'
+            className='scale-75 rotate-90 -translate-y-2 transition-all active:translate-y-0 drop-shadow-[10px_0_5px_rgba(0,0,0,0.5)] active:drop-shadow-[0px_0_5px_rgba(0,0,0,0.5)] active:scale-[70%] hover:scale-[80%] hover:-translate-y-4 hover:drop-shadow-[18px_0_5px_rgba(0,0,0,0.5)]'
+          />
+        </div>
+        <div className=''>
+          <img
+            src='/images/arrow.png'
+            onClick={moveDuck}
+            alt='up arrow'
+            className='scale-75 rotate-180 -translate-y-2 transition-all active:translate-y-0 drop-shadow-[0_-10px_5px_rgba(0,0,0,0.5)] active:drop-shadow-[0px_0_5px_rgba(0,0,0,0.5)] active:scale-[70%] hover:scale-[80%] hover:-translate-y-4 hover:drop-shadow-[0_-18px_5px_rgba(0,0,0,0.5)]'
+          />
+        </div>
       </div>
     </section>
   );
